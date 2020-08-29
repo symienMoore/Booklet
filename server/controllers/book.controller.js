82b24e7156dabc3f29258098ddd1195d92582d2b
symi <symien.moore@gmail.com>
2020-08-28 21:17:49 -0400

    built out book model and test route

---
+++ server/controllers/book.controller.js
@@ -0,0 +1,5 @@
+express = require('express');
+
+exports.bookTest = (req, res) => {
+    res.send("this is a test for the books route");
+}
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,3 +1,18 @@
+/********** routes ***********/
+/*
+    kept seperate for orginizational
+    purposes, in an attempt to keep 
+    backend files as structured as
+    possible.
+
+    -symi
+*/
+
 var express = require('express');
 var router = express.Router();
+var controller = require('../controllers/book.controller');
 
+
+router.get('/book-test', controller.bookTest);
+
+module.exports = router;
